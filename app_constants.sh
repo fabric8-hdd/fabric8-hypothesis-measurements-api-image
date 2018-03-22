54343d1702b43108780f71e361bfa8e3da9b79f7
Devang Gaur <dgaur@redhat.com>
2018-03-23 00:15:37 +0530

    Dockerfile ARGS and template PARAM fixes

--- app_constants.sh
+++ app_constants.sh
@@ -1,8 +1,8 @@
 set -a
-env_script_location=https://raw.githubusercontent.com/dev-gaur/fabric8-hypothesis-core-image/master/cico_utils/setup_env.sh
-APP_NAME=openshift-hypothesis-measurements-api
+env_script_location=https://raw.githubusercontent.com/fabric8-hypothesis/fabric8-hypothesis-core-image/master/cico_utils/setup_env.sh
+APP_NAME=osio-hypothesis-measurements-api
 APP_PORT=$(grep "HYPOTHESIS_MEASUREMENTS_API_PORT" openshift/supervisor.env | cut -d'=' -f 2)
 BUILD_MACHINE_OS=centos
-os_prep_script_location=https://raw.githubusercontent.com/dev-gaur/fabric8-hypothesis-core-image/master/cico_utils/prerequisites/${BUILD_MACHINE_OS}.sh
+os_prep_script_location=https://raw.githubusercontent.com/fabric8-hypothesis/fabric8-hypothesis-core-image/master/cico_utils/prerequisites/${BUILD_MACHINE_OS}.sh
 set +a
 
